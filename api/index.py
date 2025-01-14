46d5606b5bc75b8f512ea2ff6d1a086513c0f7fb
Sebastian Shan Reinaldo <sebastianshanreinaldo@gmail.com>
2025-01-14 14:39:54 +0700

    Init

--- api/index.py
+++ api/index.py
@@ -1,24 +1,20 @@
 from os.path import dirname, basename, isfile, join
 import glob
-from flask import Flask, render_template, request, redirect, url_for, make_response,session,jsonify,Response
+from flask import Flask, render_template, request, redirect, url_for, make_response, session, jsonify, Response
 from flask_cors import CORS, cross_origin
 import os
-from flask_session import Session
-from flask_socketio import SocketIO, emit, join_room
-from hori import *
 from dotenv import load_dotenv
+from hori import *
+
 load_dotenv()
 
+app = Flask(__name__, static_folder=os.getcwd().replace('\\', "\\\\") + '/assets', template_folder='../pages', static_url_path='/assets')
 
-app = Flask(__name__, static_folder=os.getcwd().replace('\\',"\\\\")+'/assets', template_folder='../pages',static_url_path='/assets')
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-socketio = SocketIO(app)
-CORS(app, resources={r'*':{'origin':['*']}}, supports_credentials=True)
+app.config['SECRET_KEY'] = os.urandom(24)
+
+CORS(app, resources={r'*': {'origin': ['*']}}, supports_credentials=True)
 
 route = glob.glob(join(dirname(__file__) ,"*.py"))
 __all__ = [ basename(f)[:-3] for f in route if isfile(f) and not f.endswith('__init__.py')]
 for j in __all__:
     __import__(f'api.'+j)
-
